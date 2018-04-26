02a536b5c3162ec85d693aa9c49f7ee457152cf1
tychejin1218 <tychejin@naver.com>
2018-04-26 22:52:18 +0900

    문제_10871

--- Judge/src/online/judge/problem/problem_10871.java
+++ Judge/src/online/judge/problem/problem_10871.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
