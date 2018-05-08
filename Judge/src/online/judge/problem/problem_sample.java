ab28bfca20293bf52a230d6f50b52613fb7c450f
tychejin1218 <Onlinetour@DESKTOP-ADK5DIF>
2018-05-08 16:32:37 +0900

    sample 수정

--- Judge/src/online/judge/problem/problem_sample.java
+++ Judge/src/online/judge/problem/problem_sample.java
@@ -5,43 +5,51 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
+import java.util.logging.Logger;
 
 public class problem_sample {
+
+	private static final Logger logger = Logger.getLogger( problem_sample.class.getName() );
 
 	public static void main(String[] args) {
 
 		BufferedReader br = null;
 		BufferedWriter bw = null;
 		StringTokenizer st = null;
-
-		int C = 0;
-
+		
 		try {
 
 			br = new BufferedReader(new InputStreamReader(System.in));
 			bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			st = new StringTokenizer(br.readLine());
-
-			C = Integer.parseInt(st.nextToken());
-
-			bw.write(C + "\n");
+			
+			st.hasMoreTokens();
+			bw.write(st.hasMoreTokens() + "\n");
+			
+			int count = st.countTokens();
+			
+			String str = st.nextToken();
+									
+			bw.write(str + "\n");
+			bw.write(count + "\n");
+			
 			bw.flush();
 
 		} catch (Exception e) {
-			e.getMessage();
+			logger.info(e.getMessage());
 		} finally {
 			if (br != null) {
 				try {
 					br.close();
 				} catch (Exception e) {
-					e.getMessage();
+					logger.info(e.getMessage());
 				}
 			}
 			if (bw != null) {
 				try {
 					bw.close();
 				} catch (Exception e) {
-					e.getMessage();
+					logger.info(e.getMessage());
 				}
 			}
 		}
