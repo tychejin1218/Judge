ab1568bc3dc97be1aff78ee968fdfb6110386d54
tychejin1218 <tychejin@naver.com>
2018-07-19 13:37:42 +0900

    파일 삭제

--- Judge/src/online/judge/problem/OverrdingCatClass.java
+++
@@ -1,8 +0,0 @@
-package online.judge.problem;
-
-public class OverrdingCatClass extends OverrdingAnimalClass {
-
-	public String getMethodInfo() {
-		return "고양이 메서드입니다:)";
-	}
-}
--- Judge/src/online/judge/problem/OverrdingDogClass.java
+++
@@ -1,8 +0,0 @@
-package online.judge.problem;
-
-public class OverrdingDogClass extends OverrdingAnimalClass {
-
-	public String getMethodInfo() {
-		return "강아지 메서드입니다:)";
-	}
-}
