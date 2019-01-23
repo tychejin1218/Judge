15d8968fe5cfe1afe2b5e72dca9cf3e15fded859
tychejin1218 <tychejin@naver.com>
2019-01-23 18:24:09 +0900

    문제_1110

--- Judge/src/online/judge/problem/problem_1110.java
+++ Judge/src/online/judge/problem/problem_1110.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class problem_1110 {
 
